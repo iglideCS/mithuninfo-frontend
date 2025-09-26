869f8b0a7e7895a8329cc2bc17d7806455f3a119
mithun-ctrl <itsmithun01@gmail.com>
2025-09-27 00:17:10 +0530

    env setup for production and dev

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,8 @@
-export const API_URL = "https://real-ashely-mithun-portfolio-293d7075.koyeb.app"
+// export const API_URL = "https://real-ashely-mithun-portfolio-293d7075.koyeb.app"
+
+const isProduction = import.meta.env.VITE_API_URL
+console.log(isProduction);
+
+export const API_URL = isProduction==='production' ? "https://mithuninfo-backend.koyeb.app" : "http://localhost:8000";
+
+console.log(API_URL);
